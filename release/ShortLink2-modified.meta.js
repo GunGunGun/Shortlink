c46b2da797cfcb6554f93c5c86f70be18cd79dd3
GunGunGun <GunGunGun@users.noreply.github.com>
2025-02-01 02:10:08 +0000

    Bump

---
+++ release/ShortLink2-modified.meta.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name       Additional Bypass
+// @namespace  Violentmonkey Scripts
+// @match      *://*.offerwallmedia.com/*
+// @match      *://*.freeoseocheck.com/*
+// @match      *://*.cryptowidgets.net/*
+// @match      *://*.offers4crypto.xyz/*
+// @match      *://*.insurancegold.in/*
+// @match      *://*.webfreetools.net/*
+// @match      *://*.cookinguide.net/*
+// @match      *://*.makeupguide.net/*
+// @match      *://*.coinsvalue.net/*
+// @match      *://*.dripoffers.com/*
+// @match      *://*.adwallgate.com/*
+// @match      *://*.bitcotasks.com/*
+// @match      *://*.wiki-topia.com/*
+// @match      *://*.greenenez.com/*
+// @match      *://*.coinscap.info/*
+// @match      *://*.carstopia.net/*
+// @match      *://*.carsmania.net/*
+// @match      *://*.ewall.biz/*
+// @grant      none
+// @version    7.0
+// @author     Bloggerpemula
+// @run-at     document-start
+// @description Bypass Addition for Bypass All Shortlinks
+// @require    https://code.jquery.com/jquery-3.6.0.min.js
+// @require    https://cdnjs.cloudflare.com/ajax/libs/xhook/1.4.9/xhook.js
+// @require    https://cdnjs.cloudflare.com/ajax/libs/jimp/0.22.10/jimp.min.js
+// @downloadURL https://raw.githubusercontent.com/FiorenMas/Shortlink/release/release/ShortLink2-modified.user.js
+// @updateURL https://raw.githubusercontent.com/FiorenMas/Shortlink/release/release/ShortLink2-modified.meta.js
+// ==/UserScript==
