a630b58ec7d0c81f7335306988140fc3fe26da26
GunGunGun <GunGunGun@users.noreply.github.com>
2026-09-19 05:12:57 +0000

    Bump

---
+++ release/ShortLink3-modified.user.js
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>503 | We are very busy right now. Please try again later. | OpenUserJS</title>
+  <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="We are very busy right now. Please try again later.">
+<meta name="keywords" content="userscript, userscripts, user script, user scripts, user.js, .user.js, repository, Greasemonkey, Greasemonkey Port, Tampermonkey, Violentmonkey, JavaScript, add-ons, extensions, browser">
+<link rel="shortcut icon" href="/images/favicon.ico" >
+<link rel="apple-touch-icon" href="/images/favicon64.png" />
+<link rel="apple-touch-icon-precomposed" href="/images/favicon64.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="/images/favicon120.png" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/images/favicon120.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="/images/favicon152.png" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/favicon152.png" />
+<link rel="apple-touch-icon" sizes="167x167" href="/images/favicon167.png" />
+<link rel="apple-touch-icon-precomposed" sizes="167x167" href="/images/favicon167.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon180.png" />
+<link rel="apple-touch-icon-precomposed" sizes="180x180" href="/images/favicon180.png" />
+<link rel="apple-touch-icon" sizes="1024x1024" href="/images/favicon1024.png" />
+<link rel="apple-touch-icon-precomposed" sizes="1024x1024" href="/images/favicon1024.png" />
+
+<!-- Open Search -->
+<link href="/xml/opensearch-userscripts.xml" type="application/opensearchdescription+xml" rel="search" title="OpenUserJS Userscripts">
+<link href="/xml/opensearch-groups.xml" type="application/opensearchdescription+xml" rel="search" title="OpenUserJS Groups">
+<link href="/xml/opensearch-libraries.xml" type="application/opensearchdescription+xml" rel="search" title="OpenUserJS Libraries">
+<link href="/xml/opensearch-users.xml" type="application/opensearchdescription+xml" rel="search" title="OpenUserJS Users">
+
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" media="all" href='/redist/npm/font-awesome/css/font-awesome.min.css'>
+<link rel="stylesheet" type="text/css" media="all" href='/redist/npm/octicons/build/font/octicons.css'>
+<link rel="stylesheet" type="text/css" media="all" href="/less/bootstrap/oujs.css">
+<link rel="stylesheet" type="text/css" media="all" href="/redist/npm/highlight.js/styles/github.css">
+<link rel="stylesheet" type="text/css" media="all" href="/css/common.css">
+
+  <style>
+    .status-code {
+      font-size: 10em;
+    }
+  </style>
+</head>
+<body>
+  <nav role="navigation" class="navbar navbar-default navbar-static-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" data-toggle="collapse" data-target=".navbar-collapse-top" class="navbar-toggle"><i class="fa fa-bars"></i></button>
+      <a href="/" class="navbar-brand" title="A Presentational Userscripts Repository">OpenUserJS<span class="mode"></span></a>
+    </div>
+    <div class="navbar-collapse navbar-collapse-top collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/" title="Userscripts"><span class="visible-xs-inline"><i class="fa fa-file-code-o"></i> </span>Userscripts</a></li>
+        <li><a href="/?library=true" title="Libraries"><span class="visible-xs-inline"><i class="fa fa-file-excel-o"></i> </span>Libraries</a></li>
+        <li><a href="/groups" title="Userscript Groups"><span class="visible-xs-inline"><i class="fa fa-tag"></i> </span>Groups</a></li>
+        <li><a href="/forum"><span class="visible-xs-inline"><i class="fa fa-commenting"></i> </span>Discussions</a></li>
+        <li><a href="/users"><span class="visible-xs-inline"><i class="fa fa-user"></i> </span>Users</a></li>
+        
+        
+        <li><a href="/login">Sign In / Sign Up <i class="fa fa-sign-in"></i></a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="reminders">
+
+  
+    <div class="alert alert-warning alert-dismissible small fade in" role="alert">
+      <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+      <p><i class="fa fa-fw fa-exclamation-triangle"></i> <b>NOTICE:</b> By continued use of this site you understand and agree to the binding <a class= "alert-link" href="/about/Terms-of-Service">Terms of Service</a> and <a class="alert-link" href="/about/Privacy-Policy">Privacy Policy</a>.</p>
+    </div>
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--  <div class="alert alert-info alert-dismissible alert-autodismissible small fade in" role="alert">
+    <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+    <p><i class="fa fa-fw fa-exclamation-triangle"></i> <b>REMINDER:</b> Don't miss out reading the <a class="alert-link" href="/">descriptive</a> announcement.</p>
+  </div>-->
+
+</div>
+
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <h1 class="status-code text-center">503</h1>
+            <p class="text-center">
+            
+            
+              We are very busy right now. Please try again later.
+            
+            </p>
+            <div class="text-center">
+              <ul class="nav navpills">
+                <li><a id="action" href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer id="footer">
+  <nav role="navigation" class="navbar navbar-inverse navbar-static-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" data-toggle="collapse" data-target=".navbar-collapse-bottom" class="navbar-toggle" onclick="$('html, body').animate({scrollTop: $(document).height()}, 'slow')"><i class="fa fa-bars"></i></button>
+        <a rel="bookmark" referrerpolicy="same-origin" href="#" data-toggle="modal" data-target="#donateSiteModal" class="btn btn-donate" title="Monetary donations for this site">
+          <i class="fa fa-fw fa-money"></i><span class=""> Donate</span>
+        </a>
+        <a href="https://github.com/OpenUserJs" class="navbar-brand">&copy; 2013+ OpenUserJS</a>
+      </div>
+      <div class="navbar-collapse navbar-collapse-bottom collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="/about">About</a></li>
+          <li><a href="/about/Terms-of-Service">Terms of Service</a></li>
+          <li><a href="/about/Privacy-Policy">Privacy Policy</a></li>
+          <li><a href="/about/Digital-Millenium-Copyright-Act" title="Digital Millennium Copyright Act">DMCA</a></li>
+          <li><a href="https://github.com/OpenUserJs/OpenUserJS.org">Development</a></li>
+          <li><a href="https://github.com/orgs/OpenUserJs/people">Collaborators</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</footer>
+
+<!-- JS -->
+<script type="text/javascript" charset="UTF-8" src="/redist/npm/jquery/dist/jquery.js"></script>
+<script type="text/javascript" charset="UTF-8" src="/redist/npm/bootstrap/dist/js/bootstrap.js"></script>
+
+<div class="modal fade" id="donateSiteModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title"><i class="fa fa-fw fa-money"></i> Donate for the site OpenUserJS</h4>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to go to an external site to donate a monetary value?</p>
+        <div class="alert alert-warning" role="alert">
+          <i class="fa fa-fw fa-exclamation-triangle"></i> WARNING: Some countries laws may supersede the payment processors policy such as the GDPR and PayPal. While it is highly appreciated to donate, please check with your countries privacy and identity laws regarding privacy of information first. Use at your utmost discretion.
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-fw fa-close"></i> Cancel</button>
+        <a rel="external noreferrer noopener nofollow sponsored" referrerpolicy="same-origin" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=F359CDZSR6L7W" class="btn btn-danger" role="button" title="Send a monetary Thank You!">Donate</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script type="text/javascript">
+  (function () {
+
+    var events = 'focus resize scroll';
+    var handler = null;
+    var didCallback = false;
+
+    function callback(aEl) {
+      if (!didCallback) {
+        didCallback = true;
+
+        setTimeout(function () {
+          $('.reminders .alert-autodismissible .close').each(function () {
+            this.click();
+          });
+        }, 10000);
+      }
+    }
+
+    function isElementInViewport(aEl) {
+  var rect = null;
+
+  if (aEl instanceof jQuery) {
+    aEl = aEl[0];
+  }
+
+  rect = aEl.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && // or $(window).height()
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) // or $(window).width()
+  );
+}
+
+
+    function fireIfElementVisible(aEl, aCallback) {
+      return function () {
+        if (isElementInViewport(aEl)) {
+          $(window).off(events, handler);
+
+          aCallback(aEl);
+        }
+      }
+    }
+
+    handler = fireIfElementVisible($('.reminders'), callback);
+    $(window).on(events, handler);
+
+  })();
+</script>
+
+
+  
+  <script type="text/javascript">
+    (function () {
+      'use strict';
+
+      function onClickReload(aEv) {
+        window.location.reload(false);
+      }
+
+      function onClickClose(aEv) {
+        window.close();
+      }
+
+      function onClickGoBack(aEv) {
+        window.history.back();
+      }
+
+      function onDOMContentLoaded(aEv) {
+        var thisNode = document.querySelector('#action');
+        if (thisNode) {
+          if (history.length > 1) {
+            thisNode.innerHTML = '<i class="fa fa-arrow-circle-left"></i> Go Back';
+            thisNode.addEventListener('click', onClickGoBack);
+          } else {
+            if (window.opener) {
+              thisNode.innerHTML = '<i class="fa fa-window-close"></i> Close window';
+              thisNode.addEventListener('click', onClickClose);
+            } else {
+              thisNode.innerHTML = '<i class="fa fa-refresh"></i> Reload window';
+              thisNode.addEventListener('click', onClickReload);
+            }
+          }
+        }
+      }
+
+      document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+
+    })();
+  </script>
+  
+</body>
+</html>
